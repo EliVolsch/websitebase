e199c1c8b77a055e7131cc073c358a91bce675a1
Eli Volschenk <eli@kartoza.com>
2025-01-10 12:03:19 +0200

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def blog_home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog_home.html', {'posts': posts})
